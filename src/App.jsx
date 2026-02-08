be1b15cd9489eea4ee3630b2c25d89f50f6449c8
Sabbir Hossain <sm.sabbir9671@gmail.com>
2026-02-08 23:32:16 +0600

    project setup & project start here

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import navImg from "./assets/logo.png"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     I am a student of diploma in engineering in Computer Science & Technology; 
+    <div className="navbar bg-base-100 shadow-sm">
+  <div className="flex-1">
+    <a className="text-xl">
+      <img className="w-\[60px] h-\[60px]" src={navImg} alt="" />
+    </a>
+  </div>
+  <div className="flex items-center gap-[20px]">
+    <span>Home</span>
+    <span>Fixture</span>
+    <span>Teams</span>
+    <span>Schedules</span>
+    <span>60000000</span>
+    <span>Coin</span>
+  </div>
+</div>
     </>
   )
 }
