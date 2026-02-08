820c811851ba01f39e442c13a1ed679f2bdcc4f2
Sabbir Hossain <sm.sabbir9671@gmail.com>
2026-02-08 22:28:56 +0600

    bpl-select-players-project-start-now

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     I am a student of diploma in engineering in Computer Science & Technology; 
+    </>
+  )
+}
+
+export default App
