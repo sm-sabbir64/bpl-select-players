7e974999c4da7593accaeb6df70863578e7d5fdb
Sabbir Hossain <sm.sabbir9671@gmail.com>
2026-02-15 23:27:16 +0600

    added navbar & json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import './App.css'
 import navImg from "./assets/logo.png"
+import dollarImg from "./assets/images.jpg"
 
 function App() {
   return (
     <>
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar max-w-325 mx-auto bg-base-100 shadow-sm">
   <div className="flex-1">
     <a className="text-xl">
-      <img className="w-\[60px] h-\[60px]" src={navImg} alt="" />
+      <img className="w-15 h-15" src={navImg} alt="" />
     </a>
   </div>
-  <div className="flex items-center gap-[20px]">
-    <span>Home</span>
-    <span>Fixture</span>
-    <span>Teams</span>
-    <span>Schedules</span>
-    <span>60000000</span>
-    <span>Coin</span>
+  <div className="flex items-center gap-1">
+    <span className='text-lg'>60000</span>
+    <span className='text-lg'>Coin</span>
+    <img className="w-5.5 h-5.5" src={dollarImg} alt="" />
   </div>
 </div>
     </>
